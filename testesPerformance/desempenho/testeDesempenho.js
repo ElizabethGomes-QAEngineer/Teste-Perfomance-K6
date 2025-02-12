80aec227d5ffa3315ac6118d4cb70bf7353540fc
Elizabeth Gomes <elizabeth.gomes0014@gmail.com>
2025-02-12 10:02:52 -0300

    Teste Performance K6

--- testesPerformance/desempenho/testeDesempenho.js
+++ testesPerformance/desempenho/testeDesempenho.js
@@ -7,7 +7,7 @@ export const options = {
     thresholds: {
         checks: ['rate > 0.95'],
         http_req_failed: ['rate < 0.01'],
-        http_req_duration: ['p(95) < 500']
+        http_req_duration: ['p(95) > 500']
     }
 }
 
